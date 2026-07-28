3517f6f43c340493abeb937b6199085c58b894f2
Cris GC <cris.gcc@outlook.com>
2026-07-27 23:15:30 -0600

    Layouts básicos

---
+++ src/layouts/BlankLayout .vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div id="blank-layout">
+        <h1>Blank Layout</h1>
+        <slot />
+    </div>
+</template>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div id="main-layout">
+        <h1>Main Layout</h1>
+        <slot />
+    </div>
+</template>
