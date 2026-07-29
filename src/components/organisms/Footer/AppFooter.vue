e54f5a77fd984867c395890f525a795c20137d8e
Cris GC <cris.gcc@outlook.com>
2026-07-28 18:18:20 -0600

    Footer básico con información

--- src/components/organisms/Footer/AppFooter.vue
+++ src/components/organisms/Footer/AppFooter.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts"></script>
 
 <template>
-    <footer id="footer">
-        <h1>FOOTER</h1>
+    <footer id="footer" class="bg-footer">
+        <div class="flex items-center justify-center">
+            <p class="text-center text-md font-medium leading-7 text-white">
+                © {{ new Date().getFullYear() }}, CC Master. Todos los derechos reservados.
+            </p>
+        </div>
     </footer>
 </template>
