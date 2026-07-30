5fa087807ca2b42ef0da312941884875256bd45b
Cris GC <cris.gcc@outlook.com>
2026-07-29 19:40:56 -0600

    Creación del componente CardBasicInfo

---
+++ src/components/molecules/CardBasicInfo.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+    defineProps<{
+        title?: string;
+        icon?: string;
+        items: any[];
+    }>();
+</script>
+
+<template>
+    <article class="rounded-xl border border-border bg-surface p-4">
+        <div v-if="title || icon" class="flex items-center gap-4">
+            <div v-if="icon">
+                <i :class="[icon, 'text-2xl text-t-primary']"></i>
+            </div>
+            <div v-if="title">
+                <h4 class="text-2xl text-t-primary">{{ title }}</h4>
+            </div>
+        </div>
+        <ul class="mt-4 space-y-2">
+            <li v-for="(item, index) in items" :key="index">
+                <div class="block h-full rounded-lg border border-border bg-main p-4 hover:border-t-primary transition-colors">
+                    <slot :item="item"></slot>
+                </div>
+            </li>
+        </ul>
+    </article>
+</template>
