af3b01d32d4771c14266ba22e6909ce23586ec36
Cris GC <cris.gcc@outlook.com>
2026-08-02 20:32:49 -0600

    Creación de modal de tipificación de llamada

---
+++ src/domains/seller/components/modals/ModalClassifyCall.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+    import ModalBase from '@components/molecules/modals/ModalBase.vue';
+    import ButtonBlockIcon from '@components/atoms/buttons/ButtonBlockIcon.vue';
+    
+    const isOpen = defineModel<boolean>({ default: false });
+    const buttonSize = 'w-auto';
+    const buttonPadding = 'px-2 py-0.5';
+    const buttonTextSize = 'text-sm';
+
+    const emit = defineEmits(['closeModal']);
+
+    const classifyCall = () => {
+        alert("Se tipifico la llamada")
+        isOpen.value = false;
+    }
+</script>
+
+<template>
+    <ModalBase
+        v-model="isOpen"
+        modalId="modal-save-sale"
+        modalSize="w-1/3"
+        title="Tipificar llamada"
+        titleSize="text-xl"
+        textColor="text-t-primary"
+        :buttonSize="buttonSize"
+        :buttonPadding="buttonPadding"
+        :buttonTextSize="buttonTextSize"
+        :closeButton="false"
+    >
+        <template #modal-content>
+            <p>UJU</p>
+        </template>
+        <template #modal-footer>
+            <ButtonBlockIcon 
+                bgColor="bg-surface"
+                text="Aceptar"
+                :size="buttonSize"
+                :padding="buttonPadding"
+                :textSize="buttonTextSize"
+                @click="classifyCall()"
+            />  
+        </template>
+    </ModalBase>
+</template>
