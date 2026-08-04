666e4e3be9bc49e39cf5b0023b33223a6ce41c23
Cris GC <cris.gcc@outlook.com>
2026-08-03 19:21:53 -0600

    Páginas muestra de admin

---
+++ src/domains/admin/pages/Sales.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+//
+</script>
+
+<template>
+    <div id="sales" class="container mx-auto mb-10">
+        <h1 class="text-t-primary text-5xl mb-4">Ventas</h1>
+    </div>
+</template>
---
+++ src/domains/admin/pages/SalesDashboard.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+//
+</script>
+
+<template>
+    <div id="sales-dashboard" class="container mx-auto mb-10">
+        <h1 class="text-t-primary text-5xl mb-4">Dashboard de ventas</h1>
+    </div>
+</template>
