a91b7ae261562245f0833a564576ea3528d6c03d
Cris GC <cris.gcc@outlook.com>
2026-08-05 12:05:55 -0600

    Modificación de fecha

--- src/domains/seller/pages/StatisticsPage.vue
+++ src/domains/seller/pages/StatisticsPage.vue
@@ -10,7 +10,7 @@
 <template>
     <div id="statistics" class="container mx-auto mb-10">
         <h1 class="text-t-primary text-5xl mb-4">Estadísticas</h1>
-        <h3 class="text-t-secondary text-2xl mb-4">Resumen <span class="font-semibold">{{ customFormatDate(currentDate) }}</span></h3>
+        <h3 class="text-t-secondary text-2xl mb-4">Resumen <span class="font-semibold">{{ customFormatDate(currentDate, 'of') }}</span></h3>
         <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
             <div>
                 <CardInfComponent />
