cd2f8d172d2c5f4563a51088b98f6d30b9dc8339
Cris GC <cris.gcc@outlook.com>
2026-08-05 12:05:26 -0600

    Actualización script formato de fecha

--- src/utils/dateFormat.ts
+++ src/utils/dateFormat.ts
@@ -1,4 +1,4 @@
-const customFormatDate = (currentDate: Date) => {
+const customFormatDate = (currentDate: Date, format: null | string = null) => {
     const spanishMonths = [
         'Enero',
         'Febrero',
@@ -13,11 +13,37 @@ const customFormatDate = (currentDate: Date) => {
         'Noviembre',
         'Diciembre'
     ];
+    const spanishShortMonths = [
+        'ene',
+        'feb',
+        'mar',
+        'abr',
+        'may',
+        'jun',
+        'jul',
+        'ago',
+        'sep',
+        'oct',
+        'nov',
+        'dic'
+    ];
     const date = new Date(currentDate);
     const day = String(date.getDate()).padStart(2, '0');
     const month = spanishMonths[date.getMonth()];
+    const monthShort = spanishShortMonths[date.getMonth()];
     const year = date.getFullYear();
-    return `${day} de ${month} de ${year}`;
+    let finalFormat: null | string = null;
+
+    if (format == 'of') {
+        finalFormat = `${day} de ${month} de ${year}`;
+    }
+    else if (format == 'diag') {
+        finalFormat = `${day}/${monthShort}/${year}`;
+    }
+    else {
+        finalFormat = `${day}-${monthShort}-${year}`;
+    }
+    return finalFormat;
 }
 
 export {
