f445a4846d4746a03e15d5e71b8a306f5f3ee2d0
Cris GC <cris.gcc@outlook.com>
2026-08-09 17:27:51 -0600

    Ajuste login mobile

--- src/domains/auth/pages/LoginPage.vue
+++ src/domains/auth/pages/LoginPage.vue
@@ -8,7 +8,7 @@
     <div class="bg-cover bg-no-repeat bg-center" :style="`background-image: url('${mainBanner}');`">
         <div class="flex justify-center h-screen">
             <div class="hidden lg:block lg:w-2/3 h-full"></div>
-            <div class="flex items-center w-full max-w-md px-6 ml-auto lg:w-2/6 bg-black opacity-80">
+            <div class="flex items-start lg:items-center w-full max-w-md px-6 ml-auto lg:w-2/6 bg-black opacity-80 pt-24 lg:pt-0">
                 <FormLogin />
             </div>
         </div>
